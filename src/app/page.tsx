3216d5c53e1cb868d550c778c62c04e47abbc568
Max <maxirobles1234@gmail.com>
2024-12-15 01:21:39 +0100

    add logic for PWA

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import ExerciseForm from "@/components/formComponent/ExerciseForm";
 const Home = () => {
 	return (
 		<div className="flex flex-col h-screen w-screen">
-			<div className="bg-green-200 justify-center items-center flex">
+			<div className="bg-green-200 justify-center items-center flex p-2">
 				<h1>Gym Tracker</h1>
 			</div>
 			<ExerciseForm />
