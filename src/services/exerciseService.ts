cd73587dbf77f4a5a24d53ce6135fda89f69df2e
Max <maxirobles1234@gmail.com>
2024-12-15 17:57:04 +0100

    fix for build

--- src/services/exerciseService.ts
+++ src/services/exerciseService.ts
@@ -1,6 +1,8 @@
 import { collection, onSnapshot } from "firebase/firestore";
 import { db } from "../../firebase/firebase";
 
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export const fetchUsers = (setUsers: any) => {
 	const usersRef = collection(db, "users");
 	const unsubscribe = onSnapshot(usersRef, (snapshot) => {
