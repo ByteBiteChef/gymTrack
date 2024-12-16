caa74e1a3892c86431fe25133b366ca33f3970b7
Max <maxirobles1234@gmail.com>
2024-12-16 13:13:01 +0100

    add logo

--- src/app/progress/page.tsx
+++ src/app/progress/page.tsx
@@ -1,10 +1,17 @@
 import ProgressViewer from "@/components/ProgressViewer";
 import Link from "next/link";
+import Image from "next/image";
+
 const Progress = () => {
 	return (
 		<div className="flex flex-col h-screen w-screen">
-			<div className="bg-background justify-center items-center flex p-2">
-				<h1>Gym Tracker</h1>
+			<div className="bg-gray-950 justify-center items-center flex">
+				<Image
+					src="/gym-tracker-logo.png"
+					alt="App Logo"
+					width={250}
+					height={200}
+				/>
 			</div>
 			<ProgressViewer />
 			<div className="flex w-full items-center justify-center">
