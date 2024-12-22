41d5603dfada59a66b0bc16af8a8e68569b4f8f2
Max <maxirobles1234@gmail.com>
2024-12-22 18:35:11 +0100

    add styles to chart calories

--- src/components/CaloriesGoalChart.tsx
+++ src/components/CaloriesGoalChart.tsx
@@ -1,5 +1,7 @@
 import { IDailyCalories } from "@/services/types";
 import React, { useState } from "react";
+import { MdEdit } from "react-icons/md";
+
 interface CaloriesGoalChartProps {
 	dailyCalories: IDailyCalories[];
 }
@@ -19,46 +21,61 @@ const CaloriesGoalChart: React.FC<CaloriesGoalChartProps> = ({
 		(total, entry) => total + entry.amountOfCalories,
 		0
 	);
-	console.log(totalDayCalories);
-	console.log(caloriesGoal);
 
-	const progress = (totalDayCalories / caloriesGoal) * 200;
-	console.log(progress);
+	const progress = (totalDayCalories / caloriesGoal) * 300;
+
 	return (
-		<div>
-			<div
-				className="bg-red-200 h-6 rounded-full m-2 overflow-hidden"
-				style={{ width: `200px` }}
-			>
+		<div className="flex flex-col items-center justify-center">
+			<div className="mt-4">
+				{!caloriesGoal ? (
+					<div className="flex items-center justify-center gap-1">
+						<input
+							type="number"
+							placeholder="Today's Goal"
+							className="w-2/3 rounded-md rounded-sm appearance-none focus:outline-none focus:ring-2 focus:ring-orange-500"
+							value={inputValue}
+							onChange={(e) => setInputValue(e.target.value)}
+						/>
+						<button
+							onClick={() => {
+								const numericValue =
+									parseInt(inputValue, 10) || 0;
+								setCaloriesGoal(numericValue);
+							}}
+							className="w-6 h-6 text-center text-sm uppercase transition duration-500 bg-gradient-to-r from-[#FF512F] via-[#F09819] to-[#FF512F] bg-[length:200%] bg-left text-white rounded-sm font-bold shadow-[0_0_14px_-7px_#f09819] border-0 hover:bg-right active:scale-95"
+						>
+							+
+						</button>
+					</div>
+				) : (
+					<div className="flex items-center justify-center gap-4">
+						<p className="text-orange-500 font-bold">
+							Today&apos;s Goal: {caloriesGoal}kcal
+						</p>
+						<button
+							onClick={() => {
+								setCaloriesGoal(0);
+							}}
+							className="w-6 h-6 text-lg flex items-center justify-center uppercase transition duration-500 bg-gradient-to-r from-[#FF512F] via-[#F09819] to-[#FF512F] bg-[length:200%] bg-left text-white rounded-full font-bold shadow-[0_0_14px_-7px_#f09819] border-0 hover:bg-right active:scale-95"
+						>
+							<MdEdit />
+						</button>
+					</div>
+				)}
+			</div>
+			{caloriesGoal ? (
 				<div
-					className="bg-blue-500 h-full transition-all duration-300"
-					style={{
-						width: `${progress}px`,
-					}}
-				/>
-			</div>
-			<div>
-				<input
-					type="number"
-					placeholder="Today's Goal"
-					className="w-1/2"
-					value={inputValue}
-					onChange={(e) => setInputValue(e.target.value)}
-				/>
-
-				<button
-					onClick={() => {
-						// parseInt to convert the inputValue (string) to a number
-						const numericValue = parseInt(inputValue, 10) || 0;
-						setCaloriesGoal(numericValue);
-					}}
-					className="w-6 h-6 text-center text-sm uppercase transition duration-500 bg-gradient-to-r from-[#FF512F] via-[#F09819] to-[#FF512F] bg-[length:200%] bg-left text-white rounded-full font-bold shadow-[0_0_14px_-7px_#f09819] border-0 hover:bg-right active:scale-95"
+					className="h-3 bg-gray-900 h-6 rounded-full m-2 overflow-hidden border border-gray-400"
+					style={{ width: `300px` }}
 				>
-					submit
-				</button>
-
-				<p>Current goal: {caloriesGoal}</p>
-			</div>
+					<div
+						className="rounded-full h-full transition-all duration-300 bg-gradient-to-r from-[#FF512F] via-[#F09819] to-[#FF512F] bg-[length:200%] bg-left shadow-lg"
+						style={{
+							width: `${progress}px`,
+						}}
+					/>
+				</div>
+			) : null}
 		</div>
 	);
 };
--- src/components/UserSelect.tsx
+++ src/components/UserSelect.tsx
@@ -18,7 +18,7 @@ const UserSelectInput: React.FC<UserSelectInputProps> = ({
 			<select
 				value={currentUser}
 				onChange={handleUserChange}
-				className="border border-gray-300 bg-white text-gray-800 p-1 w-full rounded-md appearance-none focus:outline-none focus:ring-2 focus:ring-orange-500"
+				className="border border-gray-300 bg-white text-gray-800 p-1 w-full rounded-sm appearance-none focus:outline-none focus:ring-2 focus:ring-orange-500"
 			>
 				<option value="" disabled>
 					Who&apos;s there?
@@ -32,7 +32,7 @@ const UserSelectInput: React.FC<UserSelectInputProps> = ({
 
 			<button
 				onClick={handleCloseSelect}
-				className="w-auto ml-2 px-2 text-center text-xs uppercase transition duration-500 bg-gradient-to-r from-[#FF512F] via-[#F09819] to-[#FF512F] bg-[length:200%] bg-left text-white rounded-md font-bold shadow-[0_0_14px_-7px_#f09819] border-0 hover:bg-right active:scale-95"
+				className="w-auto ml-1 px-2 text-center text-xs uppercase transition duration-500 bg-gradient-to-r from-[#FF512F] via-[#F09819] to-[#FF512F] bg-[length:200%] bg-left text-white rounded-sm font-bold shadow-[0_0_14px_-7px_#f09819] border-0 hover:bg-right active:scale-95"
 			>
 				x
 			</button>
