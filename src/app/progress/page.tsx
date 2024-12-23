5966db6e2b00c3a94050a35b40677ea6eb0a9b1d
Max <maxirobles1234@gmail.com>
2024-12-23 22:12:24 +0100

    change icons size

--- src/app/progress/page.tsx
+++ src/app/progress/page.tsx
@@ -6,7 +6,7 @@ const Progress = () => {
 		<div className="flex flex-col h-screen w-screen bg-[#0a0a0a] overflow-scroll pb-28">
 			<div className="flex justify-center py-4">
 				<Image
-					src="/gym-stats-logo.png"
+					src="/gym-stats.png"
 					alt="App Logo"
 					width={200}
 					height={100}
