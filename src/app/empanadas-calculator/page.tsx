a23eae028ce23f74d776afa7ce0bc0be5c28a855
Max <maxirobles1234@gmail.com>
2025-01-10 22:33:04 +0100

    secret project

--- src/app/empanadas-calculator/page.tsx
+++ src/app/empanadas-calculator/page.tsx
@@ -62,100 +62,101 @@ const CalculateEmpanadasPage = () => {
 			<h1 className="text-white text-2xl mb-6">
 				Calculadora de Empanadas
 			</h1>
-
-			<form
-				onSubmit={handleSubmit}
-				className="flex flex-col items-center space-y-4 bg-gray-800 p-4 rounded"
-			>
-				<label className="text-white" htmlFor="docenas">
-					Cantidad de docenas:
-				</label>
-				<input
-					id="docenas"
-					type="number"
-					className="p-2 border rounded text-black"
-					placeholder="Ej.: 5"
-					value={docenas}
-					onChange={(e) => setDocenas(e.target.value)}
-				/>
-
-				<button
-					type="submit"
-					className="w-full h-full rounded-sm p-2 text-center text-sm uppercase transition duration-500 bg-gradient-to-r from-[#FF512F] via-[#F09819] to-[#FF512F] bg-[length:200%] bg-left text-white font-bold shadow-[0_0_14px_-7px_#f09819] border-0 hover:bg-right active:scale-95"
+			<div className="border-orange-400 w-5/6 border-2 rounded-md p-4">
+				<form
+					onSubmit={handleSubmit}
+					className="flex flex-col items-center space-y-4 bg-white p-4 rounded w-full "
 				>
-					Calcular
-				</button>
-			</form>
+					<label htmlFor="docenas">Cantidad de docenas:</label>
+					<input
+						id="docenas"
+						type="number"
+						className="p-2 border rounded text-black"
+						placeholder="Ej.: 5"
+						value={docenas}
+						onChange={(e) => setDocenas(e.target.value)}
+					/>
 
-			{result && (
-				<div className="mt-8 text-white bg-gray-800 p-4 rounded w-80 flex flex-col">
-					<div className="w-full justify-end">
-						<ActionButton
-							className="rounded-sm w-8"
-							text="X"
-							handleAction={() => {
-								setResult(null);
-							}}
-						/>
+					<button
+						type="submit"
+						className="w-2/3 h-full rounded-sm p-2 text-center text-sm uppercase transition duration-500 bg-gradient-to-r from-[#FF512F] via-[#F09819] to-[#FF512F] bg-[length:200%] bg-left text-white font-bold shadow-[0_0_14px_-7px_#f09819] border-0 hover:bg-right active:scale-95"
+					>
+						Calcular
+					</button>
+				</form>
+
+				{result && (
+					<div className="mt-8 bg-white p-4 rounded w-full flex flex-col">
+						<div className="w-full flex justify-end">
+							<ActionButton
+								className="rounded-sm w-8"
+								text="X"
+								handleAction={() => {
+									setResult(null);
+									setDocenas("");
+								}}
+							/>
+						</div>
+
+						<h2 className="text-lg mb-2 mt-4">
+							Resultado para {docenas} docenas
+						</h2>
+						<p>Total de Empanadas: {result.totalDeEmpanadas}</p>
+
+						<div className="mt-4 border-2 p-4 rounded">
+							<h3 className="font-semibold">Masa:</h3>
+							<ul className="list-disc list-inside space-y-2 mt-2">
+								<li className="flex items-center justify-between">
+									<span>Harina: {result.masa.harina}</span>
+									<input type="checkbox" />
+								</li>
+								<li className="flex items-center justify-between">
+									<span>Agua: {result.masa.agua}</span>
+									<input type="checkbox" />
+								</li>
+								<li className="flex items-center justify-between">
+									<span>Grasa: {result.masa.grasa}</span>
+									<input type="checkbox" />
+								</li>
+								<li className="flex items-center justify-between">
+									<span>Sal: {result.masa.sal}</span>
+									<input type="checkbox" />
+								</li>
+							</ul>
+						</div>
+
+						<div className="mt-4 border-2 p-4 rounded">
+							<h3 className="font-semibold">Relleno:</h3>
+							<ul className="list-disc list-inside space-y-2 mt-2">
+								<li className="flex items-center justify-between">
+									<span>Carne: {result.relleno.carne}</span>
+									<input type="checkbox" />
+								</li>
+								<li className="flex items-center justify-between">
+									<span>Huevos: {result.relleno.huevos}</span>
+									<input type="checkbox" />
+								</li>
+								<li className="flex items-center justify-between">
+									<span>Verdeo: {result.relleno.verdeo}</span>
+									<input type="checkbox" />
+								</li>
+								<li className="flex items-center justify-between">
+									<span>
+										Grasa Cocción:{" "}
+										{result.relleno.grasaCoccion}
+									</span>
+									<input type="checkbox" />
+								</li>
+							</ul>
+						</div>
+
+						<div className="mt-4 border-2 p-4 rounded">
+							<span className="font-semibold">Grasa Total:</span>{" "}
+							{result.grasaTotal}
+						</div>
 					</div>
-
-					<h2 className="text-lg mb-2 mt-4">
-						Resultado para {docenas} docenas
-					</h2>
-					<p>Total de Empanadas: {result.totalDeEmpanadas}</p>
-
-					<div className="mt-4">
-						<h3 className="font-semibold">Masa:</h3>
-						<ul className="list-disc list-inside space-y-2 mt-2">
-							<li className="flex items-center justify-between">
-								<span>Harina: {result.masa.harina}</span>
-								<input type="checkbox" />
-							</li>
-							<li className="flex items-center justify-between">
-								<span>Agua: {result.masa.agua}</span>
-								<input type="checkbox" />
-							</li>
-							<li className="flex items-center justify-between">
-								<span>Grasa: {result.masa.grasa}</span>
-								<input type="checkbox" />
-							</li>
-							<li className="flex items-center justify-between">
-								<span>Sal: {result.masa.sal}</span>
-								<input type="checkbox" />
-							</li>
-						</ul>
-					</div>
-
-					<div className="mt-4">
-						<h3 className="font-semibold">Relleno:</h3>
-						<ul className="list-disc list-inside space-y-2 mt-2">
-							<li className="flex items-center justify-between">
-								<span>Carne: {result.relleno.carne}</span>
-								<input type="checkbox" />
-							</li>
-							<li className="flex items-center justify-between">
-								<span>Huevos: {result.relleno.huevos}</span>
-								<input type="checkbox" />
-							</li>
-							<li className="flex items-center justify-between">
-								<span>Verdeo: {result.relleno.verdeo}</span>
-								<input type="checkbox" />
-							</li>
-							<li className="flex items-center justify-between">
-								<span>
-									Grasa Cocción: {result.relleno.grasaCoccion}
-								</span>
-								<input type="checkbox" />
-							</li>
-						</ul>
-					</div>
-
-					<p className="mt-4">
-						<span className="font-semibold">Grasa Total:</span>{" "}
-						{result.grasaTotal}
-					</p>
-				</div>
-			)}
+				)}
+			</div>
 		</div>
 	);
 };
